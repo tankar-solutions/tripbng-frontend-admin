bc1746c7d116938c741f006d13f80e9041390c9e
nikhilnigamnik <nikhilnigamnik@gmail.com>
2024-08-02 09:44:50 +0530

    init

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Button from '@/components/ui/button'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <Button>
+        Hello
+      </Button>
+    </div>
+  )
+}
