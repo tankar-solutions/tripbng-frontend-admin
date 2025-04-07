14d1a1718fbbc83373250d35c40d7f19c052ae40
Tankar Solutions Private Limited <tankarsolutionsprivatelimited@gmail.com>
2025-04-07 18:11:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+const host = new URL("https://admin.tripbng.com").hostname;
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [host],
+   }
 })
